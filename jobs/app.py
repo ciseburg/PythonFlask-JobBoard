4268761e08affe1c46b1de479a7d47f4a4e0fc45
Chitturi,Kiran <kiran_chitturi@vanguard.com>
2021-12-04 19:34:13 -0600

    display indv jobs - Step6

--- jobs/app.py
+++ jobs/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, g 
-#global helper to provide access to the database throughout the application
+from flask import Flask, render_template, g
+# global helper to provide access to the database throughout the application
 import sqlite3
 
 PATH = "db/jobs.sqlite"
 app = Flask(__name__)
+
 
 def open_connection():
     connection = getattr(g, '_connection', None)
@@ -11,6 +12,7 @@ def open_connection():
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
+
 
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
@@ -22,18 +24,25 @@ def execute_sql(sql, values=(), commit=False, single=False):
     cursor.close()
     return results
 
+
 @app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
     if connection != None:
         connection.close()
 
+
 @app.route("/")
 @app.route("/jobs")
 def jobs():
     # return "Hello World!"
-    # sqlQuery = 'SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id'
-    # jobs = execute_sql(sqlQuery)
-    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
-    return render_template('index.html',jobs=jobs)
+    jobs = execute_sql(
+        'SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
+    return render_template('index.html', jobs=jobs)
 
+
+@app.route("/job/<job_id>")
+def job(job_id):
+    job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [
+                      job_id], single=True)
+    return render_template('job.html', job=job)
